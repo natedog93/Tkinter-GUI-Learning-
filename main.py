58e7e7eff20d85f8103bbf7f5181ab75854b388e
nmojica <nmlogon2700@gmail.com>
2025-08-26 22:33:41 -0700

    How to configure and change properties of a particular component. How to import all classes using the *.

--- main.py
+++ main.py
@@ -1,14 +1,22 @@
-import tkinter
+# import tkinter
+#Alternatively, you could do the below to import all classes - Don't need the tkinter. when initializing anymore
+from tkinter import *
 
-window = tkinter.Tk()
+window = Tk()
 window.title("My First GUID Program")
 window.wm_minsize(width=500, height=300) #will auto adjust depending on what you have within the window - Default if starting
 
 #componnent use
-my_label = tkinter.Label(text="I Am a Label", font=("Arial", 24, "bold")) #doesn't display automatically
+my_label = Label(text="I Am a Label", font=("Arial", 24, "bold")) #doesn't display automatically
 # create the label then specify how it is laid out on screen. Notice that parameters do now show within parenthesis
 my_label.pack(side= "left") #Automatically centers text on the screen, if you create a second Object, it will populate below this
 
+#How we configure or change the properties of a particular component that we have created
+my_label["text"] = "New Text"
+my_label.configure(text="New Text")
+
+#Can also create buttons
+button = Button
 
 
 
